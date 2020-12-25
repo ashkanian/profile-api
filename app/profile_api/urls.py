53be89b62076dba9da0b8037db6a5a14449863f0
Ashkan <ashkankari@gmail.com>
2020-12-25 11:06:48 +0330

    completing APIView and ViewSet

--- app/profile_api/urls.py
+++ app/profile_api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from profile_api.views import HelloApiView
+from profile_api.views import HelloViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('hello-viewset', HelloViewSet, basename='viewset')
 
 urlpatterns = [
-    path('profile/', HelloApiView.as_view())
+    path('profile/', HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
--- app/profile_api/views.py
+++ app/profile_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
+from rest_framework import viewsets
 
+# APIView
 class HelloApiView(APIView):
     '''Testing api view'''
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         '''RETURNS a list of APIVIEW'''
@@ -16,4 +21,38 @@ class HelloApiView(APIView):
         return Response({
             'messages': 'hello!',
             'api view': an_apiview
+        })
+
+    def post(self, request):
+        '''Creates a hello message with our name'''
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+
+            return Response({
+                'message': message
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        '''update whole object using put request'''
+        return Response({'method': 'Is a put request'})
+
+# Viewset
+class HelloViewSet(viewsets.ViewSet):
+    '''Testing viewset api'''
+    def list(self, request):
+        '''get a list from api'''
+        a_viewset = [
+            'uses some actions',
+            'it is very good',
+            'it is very awful',
+        ]
+
+        return Response({
+            'message': 'hello world',
+            'a_viewset': a_viewset
         })
